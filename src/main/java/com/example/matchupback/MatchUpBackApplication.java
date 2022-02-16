029c981c416087318d326e0fffdfd56d9ad280ca
Candy <candymdeoleo@gmail.com>
2022-02-16 13:02:34 +0100

    inicial commit

---
+++ src/main/java/com/example/matchupback/MatchUpBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchupback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchUpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchUpBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matchupback/MatchUpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchupback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchUpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
