77757ceec6980be3b2c6eef8d5607963e0714f79
Candy <candymdeoleo@gmail.com>
2022-02-16 22:20:16 +0100

    Añadido test unitarios

--- src/test/java/com/example/matchupback/UserAdTest.java
+++ src/test/java/com/example/matchupback/UserAdTest.java
@@ -1,10 +1,22 @@
 package com.example.matchupback;
 
+import com.example.matchupback.Model.UserAd;
+import com.example.matchupback.Repositories.UserAdRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class UserAdTest {
 
     @Test
     void returnsUserAdPostersName() {
+        UserAd userAd = new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h");
+        assertThat(userAd.getName(), equalTo("Carlos Perez"));
     }
 }
