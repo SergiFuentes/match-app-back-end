2ae5d52f1c89e0f1ff06aaff504ae3e481bd4a13
Candy <candymdeoleo@gmail.com>
2022-02-16 22:36:53 +0100

    añadidos anuncios en sampledataloader

--- src/main/java/com/example/matchupback/Repositories/SampleDataLoader.java
+++ src/main/java/com/example/matchupback/Repositories/SampleDataLoader.java
@@ -20,7 +20,10 @@ public class SampleDataLoader {
     @PostConstruct
     public void loadSampleData() {
         userAdRepository.saveAll(List.of(
-                new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h")
+                new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h"),
+                new UserAd(2L,"Juan Gonzalez", "https://images.unsplash.com/photo-1644994141078-4f60ef42aa78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80", "Colombia", "description", "18h-20h"),
+                new UserAd(3L,"Miquel Casas", "https://images.unsplash.com/photo-1644896026815-76216723bb47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", "Bilbao", "description", "14h-18h"),
+                new UserAd(4L,"Juana Rodriguez", "https://images.unsplash.com/photo-1644944693376-de2411390a44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", "Gijón", "description", "10h-13h")
 
         ));
     }
