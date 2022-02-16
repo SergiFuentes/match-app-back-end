b5cb3eb88a1f53a0e10752f70f8a14e3f7806e93
SergiFuentes <you@example.com>
2022-02-16 22:42:03 +0100

    doing getters tests

--- src/test/java/com/example/matchupback/UserAdTest.java
+++ src/test/java/com/example/matchupback/UserAdTest.java
@@ -1,12 +1,7 @@
 package com.example.matchupback;
 
 import com.example.matchupback.Model.UserAd;
-import com.example.matchupback.Repositories.UserAdRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -15,8 +10,33 @@ import static org.hamcrest.Matchers.equalTo;
 public class UserAdTest {
 
     @Test
+    void returnsUserAdPostersId() {
+        UserAd userAd = new UserAd(2L, "Carlos Perez", "no-image", "Barcelona", "description", "19h-20h");
+        assertThat(userAd.getId(), equalTo(2L));
+    }
+    @Test
     void returnsUserAdPostersName() {
         UserAd userAd = new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h");
         assertThat(userAd.getName(), equalTo("Carlos Perez"));
+    }
+    @Test
+    void returnsUserAdPostersImage() {
+        UserAd userAd = new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h");
+        assertThat(userAd.getImage(), equalTo("no-image"));
+    }
+    @Test
+    void returnsUserAdPostersLocation() {
+        UserAd userAd = new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h");
+        assertThat(userAd.getLocation(), equalTo("Barcelona"));
+    }
+    @Test
+    void returnsUserAdPostersDescription() {
+        UserAd userAd = new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h");
+        assertThat(userAd.getDescription(), equalTo("description"));
+    }
+    @Test
+    void returnsUserAdPostersTime() {
+        UserAd userAd = new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h");
+        assertThat(userAd.getTime(), equalTo("19h-20h"));
     }
 }
