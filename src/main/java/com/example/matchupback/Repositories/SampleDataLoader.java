7dff75fde1769465518e914041d1a27a61ddc98d
SergiFuentes <you@example.com>
2022-02-16 19:59:00 +0100

    testing in progress...

--- src/main/java/com/example/matchupback/Repositories/SampleDataLoader.java
+++ src/main/java/com/example/matchupback/Repositories/SampleDataLoader.java
@@ -20,7 +20,10 @@ public class SampleDataLoader {
     @PostConstruct
     public void loadSampleData() {
         userAdRepository.saveAll(List.of(
-                new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h")
+                new UserAd(1L,"Carlos Perez", "no-image", "Barcelona", "description", "19h-20h"),
+                new UserAd(2L,"Juan Gonzalez", "no-image", "Colombia", "description", "18h-20h"),
+                new UserAd(3L,"Carolta Perez", "no-image", "Bilbao", "description", "14h-18h"),
+                new UserAd(4L,"Juana Perez", "no-image", "Gijón", "description", "10h-13h")
 
         ));
     }
--- src/test/java/com/example/matchupback/MatchUpBackApplicationTests.java
+++ src/test/java/com/example/matchupback/MatchUpBackApplicationTests.java
@@ -1,22 +1,13 @@
 package com.example.matchupback;
 
-import com.example.matchupback.Model.UserAd;
 import com.example.matchupback.Repositories.UserAdRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import java.util.List;
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,24 +24,49 @@ class MatchUpBackApplicationTests {
 
         @BeforeEach
         void setUp() {
-            userAdRepository.deleteAll();
         }
 
     @Test
     public void test() throws Exception {
 
-        String expectedJson = "[{\n" +
+        String expectedJson = "[    {\n" +
                 "        \"id\": 1,\n" +
                 "        \"name\": \"Carlos Perez\",\n" +
                 "        \"image\": \"no-image\",\n" +
                 "        \"location\": \"Barcelona\",\n" +
                 "        \"description\": \"description\",\n" +
                 "        \"time\": \"19h-20h\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"name\": \"Juan Gonzalez\",\n" +
+                "        \"image\": \"no-image\",\n" +
+                "        \"location\": \"Colombia\",\n" +
+                "        \"description\": \"description\",\n" +
+                "        \"time\": \"18h-20h\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 3,\n" +
+                "        \"name\": \"Carolta Perez\",\n" +
+                "        \"image\": \"no-image\",\n" +
+                "        \"location\": \"Bilbao\",\n" +
+                "        \"description\": \"description\",\n" +
+                "        \"time\": \"14h-18h\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 4,\n" +
+                "        \"name\": \"Juana Perez\",\n" +
+                "        \"image\": \"no-image\",\n" +
+                "        \"location\": \"Gijón\",\n" +
+                "        \"description\": \"description\",\n" +
+                "        \"time\": \"10h-13h\"\n" +
                 "    }]";  //Create JSON of expected result as a string
 
         mockMvc.perform(get("/ads"))
                 .andExpect(status().isOk()) //Check the status code of the response
-                .andExpect(content().json(expectedJson)); //Check the JSON of the response
+                .andExpect(content().json(expectedJson)) //Check the JSON of the response
+                .andExpect(jsonPath("$.length()").value("4"))
+                .andExpect(jsonPath("$.[1].id").value("2"));
     }
 }
 
